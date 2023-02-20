36f4602bb08b6675db5c9004f88788ebdd7f0319
Sorin Marta <sorinmarta0@gmail.com>
2023-02-20 11:44:00 +0200

    Improved loader

--- app/lib/wppl-loader.php
+++ app/lib/wppl-loader.php
@@ -4,10 +4,9 @@ if(!class_exists('WPPL_Loader')) {
 	class WPPL_Loader {
 		public function __construct() {
 			$this->lib();
-            $this->help();
-            $this->traits();
-            $this->interfaces();
+			$this->help();
             $this->abstracts();
+            $this->interfaces();
 			$this->models();
 			$this->controllers();
 		}
@@ -15,70 +14,54 @@ if(!class_exists('WPPL_Loader')) {
 		/**
 		 * Requires all the library files
 		 */
-		private function lib(): void
-        {
+		private function lib() {
 			foreach ( $this->get_files( WPPL_LIB ) as $file ) {
-				require_once WPPL_LIB . "/$file";
+				require_once $file;
 			}
 		}
 
 		/**
 		 * Requires all the helpers
 		 */
-		private function help(): void
-        {
+		private function help() {
 			foreach ( $this->get_files( WPPL_HELPER ) as $file ) {
-				require_once WPPL_HELPER . "/$file";
+				require_once $file;
 			}
 		}
 
 		/**
 		 * Requires all the models
 		 */
-		private function models(): void
-        {
+		private function models() {
 			foreach ( $this->get_files( WPPL_MODEL ) as $file ) {
-				require_once WPPL_MODEL . "/$file";
+				require_once $file;
 			}
 		}
 
 		/**
 		 * Requires all the controllers
 		 */
-		private function controllers(): void
-        {
+		private function controllers() {
 			foreach ( $this->get_files( WPPL_CONTROLLER ) as $file ) {
-				require_once WPPL_CONTROLLER . "/$file";
+				require_once $file;
 			}
 		}
 
         /**
-         * Requires all the abstract classes
-         */
-        private function abstracts(): void
-        {
-            foreach ( $this->get_files( WPPL_ABSTRACT ) as $file ) {
-                require_once WPPL_ABSTRACT . "/$file";
-            }
-        }
-
-        /**
-         * Requires all the traits
-         */
-        private function traits(): void
-        {
-            foreach ( $this->get_files( WPPL_TRAIT ) as $file ) {
-                require_once WPPL_TRAIT . "/$file";
-            }
-        }
-
-        /**
          * Requires all the interfaces
          */
-        private function interfaces(): void
-        {
+        private function interfaces() {
             foreach ( $this->get_files( WPPL_INTERFACE ) as $file ) {
-                require_once WPPL_INTERFACE . "/$file";
+                require_once $file;
+            }
+        }
+
+        /**
+         * Requires all traits
+         */
+        private function abstracts() {
+            foreach ( $this->get_files( WPPL_ABSTRACT ) as $file ) {
+                require_once $file;
             }
         }
 
@@ -91,18 +74,22 @@ if(!class_exists('WPPL_Loader')) {
 		 */
 		private function get_files( $dir ): array {
 			$dir_object = new \DirectoryIterator( $dir );
-			$returnable = [];
+			$returnable = array();
 
 			foreach ( $dir_object as $file ) {
-				if ( $file->isDot() || $file->isDir() ) {
+				if ( $file->isDot() ) {
 					continue;
+				}
+
+				if ( $file->isDir() ){
+					$returnable = array_merge($returnable, $this->get_files($dir . '/' .$file));
 				}
 
 				if ( $file->getExtension() != 'php' ) {
 					continue;
 				}
 
-				$returnable[] = $file->getFilename();
+				$returnable[] = $dir . '/' .$file->getFilename();
 			}
 
 			return $returnable;
